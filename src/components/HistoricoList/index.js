75610ef54a0b5445a8ceeb05814771b4ab06ed73
ANTHONY LIMA E SILVA <72108893+Anthony-a-l-s@users.noreply.github.com>
2024-03-07 13:32:01 -0300

    listando receitas

---
+++ src/components/HistoricoList/index.js
@@ -0,0 +1,30 @@
+import { View } from 'react-native';
+import {
+    Container,
+    TipoText,
+    Tipo,
+    IconView,
+    ValorText
+} from './styles';
+
+import Icon from 'react-native-vector-icons/Feather'
+
+export default function HistoricoList({ data }) {
+    return (
+        <Container>
+            <Tipo>
+                <IconView tipo={data.type}>
+                    <Icon
+                        name={data.type === 'receita' ? 'arrow-up' : 'arrow-down'} 
+                        size={20} 
+                        color='#fff'
+                    />
+                    <TipoText>{data.type}</TipoText>
+                </IconView>
+            </Tipo>
+            <ValorText>
+                R$ {data.value}
+            </ValorText>
+        </Container>
+    );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,53 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 import { AuthContext } from '../../contexts/auth'
 import Header from '../../components/Header'
+import HistoricoList from '../../components/HistoricoList';
 
 import {
     Background,
     ListBalance,
+    Area,
+    Title,
+    List
 } from './styles';
 
 import api from '../../services/api'
 import { format } from 'date-fns'
 import { useIsFocused } from '@react-navigation/native';
 import BalanceItem from '../../components/BalanceItem';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export default function Home() {
 
     const isFocused = useIsFocused();
     const [listBalance, setListBalance] = useState([]);
     const [dateMovements, setDatemovements] = useState(new Date())
+    const [movements, setMovements] = useState([])
 
 
 
     useEffect(() => {
         let isActive = true;
+
         async function getMovements() {
+
             let dateFormated = format(dateMovements, 'dd/MM/yyyy')
+
+            const receives = await api.get('/receives',{
+                params: {
+                    date: dateFormated
+                }
+            })
             const balance = await api.get('/balance', {
                 params: {
                     date: dateFormated
                 }
             })
             if (isActive) {
-                setListBalance(balance.data)
+                setListBalance(balance.data);
+                setMovements(receives.data);
             }
         }
 
@@ -53,6 +68,21 @@ export default function Home() {
                 showsHorizontalScrollIndicator={false}
                 keyExtractor={item => item.tag}
                 renderItem={({ item }) => (<BalanceItem data={item} />)}
+            />
+
+            <Area>
+                <TouchableOpacity>
+                    <Icon name='event' color="#121212" size={30} />
+                </TouchableOpacity>
+                <Title>Ultimas movimentações</Title>
+            </Area>
+
+            <List
+                data={movements}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item }) => <HistoricoList data={item}/>}
+                contentContainerStyle={{paddingBottom: 20}}
             />
 
         </Background>
